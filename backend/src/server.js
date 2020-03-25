e8f998590b91204e20b7fcd80d2d16ba0a4a3ce2
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-25 21:32:34 +0100

    feat: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
+const cors = require('cors');
 const logger = require('./utils/logger');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3003);
+app.listen(3003, () => {
+  logger.info('Server running on port 3003');
+});
