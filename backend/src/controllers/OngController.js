2ad4408f494c62cdf9574d2e4906ad9d8ffa5578
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-25 21:33:58 +0100

    feat: add ongs controller

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,46 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+
+    return res.json({ id });
+  },
+
+  async index(req, res) {
+    const ongs = await connection('ongs').select('*');
+
+    return res.json(ongs);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const ong = await connection('ongs').where('id', id).first();
+
+    if (id !== ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' });
+    }
+
+    if (!ong) {
+      return res.status(401).json({ error: 'Ong not found.' });
+    }
+
+    await connection('ongs').where('id', id).delete();
+
+    return res.status(204).send();
+  },
+};
