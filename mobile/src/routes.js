09f2f78d03def03931a3afb0b07dcbc5ee173de0
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-26 22:26:07 +0100

    feat: add app routes

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Incidents from '~/pages/Incidents';
+import Details from '~/pages/Details';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Details" component={Details} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
