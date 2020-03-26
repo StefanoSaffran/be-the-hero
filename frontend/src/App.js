202e0d991d08308d647b1e9fd63bc883cd37f772
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-26 22:12:27 +0100

    feat: add dark mode to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,12 @@ import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+
+import light from '~/styles/themes/light';
+import dark from '~/styles/themes/dark';
+import Context from '~/styles/themes/context';
+import usePersistedState from '~/utils/usePersistedState';
 
 import './config/ReactotronConfig';
 
@@ -14,14 +20,24 @@ import { store, persistor } from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
+  const [theme, setTheme] = usePersistedState('theme', dark);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Routes />
-          <GlobalStyle />
-          <ToastContainer autoClose={3500} />
-        </Router>
+        <Context.Provider value={{ toggleTheme }}>
+          <ThemeProvider theme={theme}>
+            <Router history={history}>
+              <Routes />
+              <GlobalStyle />
+              <ToastContainer autoClose={3500} />
+            </Router>
+          </ThemeProvider>
+        </Context.Provider>
       </PersistGate>
     </Provider>
   );
