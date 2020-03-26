3fe7065c36e11df1e719224c110a7f5174a526b2
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-26 22:10:14 +0100

    feat: add theme files

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-activity/dist/react-activity.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
@@ -25,11 +26,27 @@ export default createGlobalStyle`
     font: 400 14px 'Roboto', sans-serif;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
-    background: #f0f0f5;
+    background: ${({ theme }) => theme.colors.background};
   }
 
   input, button, textarea, input::placeholder, textarea::placeholder {
     font: 400 18px 'Roboto', sans-serif;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active  {
+    -webkit-box-shadow: 0 0 0px 1000px ${({ theme }) =>
+      theme.colors.background} inset;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    -webkit-transition: "color 9999s ease-out, background-color 9999s ease-out";
+    -webkit-transition-delay: 9999s;
   }
 
   a {
@@ -41,7 +58,7 @@ export default createGlobalStyle`
     display: flex;
     align-items: center;
     margin-top: 40px;
-    color: #41414d;
+    color: ${({ theme }) => theme.colors.textStrong};
     font-size: 18px;
     font-weight: 500;
     opacity: 1;
---
+++ frontend/src/styles/themes/dark.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#E02041',
+    secundary: '#5B2333',
+
+    background: '#333',
+    CardBackground: '#222',
+    title: '#fff',
+    textStrong: '#fff',
+    text: '#ccc',
+  },
+};
