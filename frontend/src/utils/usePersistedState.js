33c2bebbd3ecf57bef370732ddfd4fb6841bc0c8
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-26 22:11:38 +0100

    feat: add hook to store theme in the localStorage

---
+++ frontend/src/utils/usePersistedState.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+export default function usePersistedState(key, initialState) {
+  const [state, setState] = useState(() => {
+    const storageValue = localStorage.getItem(key);
+
+    if (storageValue) return JSON.parse(storageValue);
+    return initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
