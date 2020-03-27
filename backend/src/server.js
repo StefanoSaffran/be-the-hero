a025c2ad8134101e5bdb7320b1bfacbf5b58b24c
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-27 20:16:34 +0100

    chore: updates to add tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,5 @@
-const express = require('express');
-
-const cors = require('cors');
+const app = require('./app');
 const logger = require('./utils/logger');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use(routes);
 
 app.listen(3003, () => {
   logger.info('Server running on port 3003');
