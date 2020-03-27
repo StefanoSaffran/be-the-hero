b4d4827e95769f1028016adebc0ae87b7e21d2e2
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-27 20:18:42 +0100

    chore: some minor updates

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
 const connection = require('../database/connection');
+const generateUniqueId = require('../utils/generateUniqueId');
 
 module.exports = {
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -4,9 +4,18 @@ module.exports = {
   async index(req, res) {
     const ong_id = req.headers.authorization;
 
+    const [count] = await connection('incidents').count();
+
     const incidents = await connection('incidents')
       .where('ong_id', ong_id)
-      .select('*');
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .select(['incidents.*', 'ongs.city', 'ongs.uf']);
+
+    res.header('X-Total-Count', count['count(*)']);
+
+    /*   const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*'); */
 
     return res.json(incidents);
   },
